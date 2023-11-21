bc5eb55dae038a8cc9e31d5ad38c32feb6ca0504
Nick <ghost2394228@gmail.com>
2023-11-21 19:51:55 +0300

    wip

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,18 +1,15 @@
 import test from 'node:test';
 import assert from 'assert/strict';
 
-import {
-  countAverage,
-  alphaOmega,
-  fill,
-  fromPairs,
-  intersection,
-} from '../index.js';
+import { isEmpty, isEmptyDeep } from '../index.js';
 
 test('task1', () => {
-  assert.strictEqual(countAverage([5, 7, 9, 3, 2]), 5.2);
+  assert.strictEqual(isEmpty({a: true, b: false}), false);
+  assert.strictEqual(isEmpty({a: undefined, b: false}), false);
+  assert.strictEqual(isEmpty({a: undefined}), true);
+  assert.strictEqual(isEmpty({}), true);
 });
-test('task2', () => {
+/* test('task2', () => {
   assert.ok(alphaOmega([5, 7, 9, 3, 2]), [5, 2]);
   assert.ok(alphaOmega([4, 7, 9, 3, 2]), [4, 2]);
 });
@@ -24,24 +21,12 @@ test('task3', () => {
     'someString',
   ]);
   assert.ok(fill(2, ['someString']), [['someString'], ['someString']]);
-});
+}); */
 test('task4', () => {
-  assert.ok(
-    fromPairs([
-      ['a', 1],
-      ['b', 2],
-    ]),
-    { a: 1, b: 2 },
-  );
-  assert.ok(
-    fromPairs([
-      ['someString', [2, 3]],
-      ['b', 2],
-    ]),
-    { someString: [2, 3], b: 2 },
-  );
+  assert.strictEqual(isEmptyDeep({ a: { b: undefined } }), true);
+  assert.strictEqual(isEmptyDeep({ a: { b: 1 } }), false);
 });
-test('task5', () => {
+/* test('task5', () => {
   assert.ok(intersection([1, 2], [2, 3]), [2]);
   assert.ok(intersection(['b', 'e', 'c'], ['b', 'b', 'e']), ['b', 'e']);
-});
+}); */
