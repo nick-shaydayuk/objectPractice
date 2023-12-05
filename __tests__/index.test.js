1a88ddcab96f4d5a2afbe556270de5b9b44abcf8
Nick <ghost2394228@gmail.com>
2023-12-05 15:37:57 +0300

    wip autograding

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -9,25 +9,25 @@ import {
   intersectionDeep,
 } from '../index.js';
 
-test('task1', () => {
+test('step1', () => {
   assert.strictEqual(isEmpty({ a: true, b: false }), false);
   assert.strictEqual(isEmpty({ a: undefined, b: false }), false);
   assert.strictEqual(isEmpty({ a: undefined }), true);
   assert.strictEqual(isEmpty({}), true);
 });
-test('task2', () => {
+test('step2', () => {
   assert.ok(without({ a: 1, b: 2, c: 3 }, 'b', 'c'), { a: 1 });
   assert.ok(without({ a: 1, b: 2, c: 3 }, 'a'), { b: 2, c: 3 });
 });
-test('task3', () => {
+test('step3', () => {
   assert.strictEqual(isEmptyDeep({ a: { b: undefined } }), true);
   assert.strictEqual(isEmptyDeep({ a: { b: 1 } }), false);
 });
-test('task4', () => {
+test('step4', () => {
   assert.ok(intersection({ a: 1, b: 2 }, { c: 1, b: 2 }), { b: 2 });
   assert.ok(intersection({ a: 1, b: 2, c: 1 }, { c: 1, b: 2 }), { c: 1, b: 2 });
 });
-test('task5', () => {
+test('step5', () => {
   assert.ok(
     intersectionDeep(
       { a: 1, b: { c: 3, d: { e: 5 } } },
